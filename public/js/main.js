ff11a68d2345f2be0925127c70ec37a0328d8a5f
Matthew Aebersold <matt.aebersold@bkwld.com>
2013-07-22 15:37:21 -0700

    removed the 20 circle cap

--- public/js/main.js
+++ public/js/main.js
@@ -18,19 +18,15 @@ define('main', function (require) {
 		_ = require('underscore'),
 		Circle = require('views/Circle');
 
-	var cap = 20,
-		count = 0;
 
 	// Main window click event (HammerJS)
 	$('.wrap').on('click', function(e) {
 		e.preventDefault();
 
-		// make it so you can only create 20 circles
-		// also, check and make sure you're not clicking on an existing circle
-		if(count < cap && !$(e.target).hasClass('circle')) {
+		// check and make sure you're not clicking on an existing circle
+		if(!$(e.target).hasClass('circle')) {
 			var circle = new Circle({x: e.pageX, y: e.pageY, size: 200});
 			$('.wrap').append(circle.$el);
-			count++;
 		}
 	});
 });
